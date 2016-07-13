0075ab1081244d310a56a844dec12f1663ac0852
inagantisrikiran <srikiran.ios@gmail.com>
2016-07-13 15:24:12 -0700

    test

---
+++ test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test
+//
+//  Created by SRIKIRAN INAGANTI on 7/13/16.
+//  Copyright © 2016 SRIKIRAN INAGANTI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
